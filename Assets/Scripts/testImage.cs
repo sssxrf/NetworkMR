994e57efbc19f78a0907e53da9d599c6d8b777fc
sssxrf <ruifengs.xu@outlook.com>
2025-04-05 12:43:54 -0500

    test download image from google drive

---
+++ Assets/Scripts/testImage.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityGoogleDrive;
+
+
+public class testImage : MonoBehaviour
+{
+
+    [SerializeField]
+    public Texture2D image;
+    public RawImage finaldownload;
+    public byte[] downloadedcontent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            StartCoroutine("downloadFromDrive");
+        }
+    }
+
+    public IEnumerator downloadFromDrive()
+    {
+        var request = GoogleDriveFiles.Download("1gMi9FEi9idpEpCGpAxog-DLamxKEQskN");
+        yield return request.Send();
+        print(request.IsError);
+        print(request.ResponseData.Content);
+
+        downloadedcontent = request.ResponseData.Content;
+        Texture2D tex = new Texture2D(1024, 1024);
+        tex.LoadImage(downloadedcontent);
+        tex.Apply();
+        finaldownload.texture = tex;   
+
+    }
+}
